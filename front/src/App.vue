bd86ee33c37a0a1b7a988e15f91f0fcdfe17d392
Yann Bodiguel <yann.bodiguel@etu.umontpellier.fr>
2026-01-10 16:14:34 +0100

    style (front): CSS Nav bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,6 +4,7 @@ import AuthComponent from './components/AuthComponent.vue';
 import { apiStore, loggedInUser } from './util/apiStore';
 import NotifList from './components/NotifList.vue';
 import { isAuthDiplayed } from './util/authDisplayedStore';
+import NavButton from './components/NavButton.vue';
 
 apiStore.refresh();
 </script>
@@ -13,12 +14,11 @@ apiStore.refresh();
   <NotifList />
   <header>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/games">Games</RouterLink>
-        <RouterLink to="/game/0">Game of id 0</RouterLink>
-        <button @click="isAuthDiplayed = true"> Login</button>
-        <span> Logged in User: {{ loggedInUser ? loggedInUser.login : 'User not logged in' }}</span>
-        <RouterLink v-if="loggedInUser" to="/account"> My Account</RouterLink>
+        <NavButton to="/games"> Games</NavButton>
+        <NavButton to="/games/0"> Game of id 0</NavButton>
+        <NavButton @click="isAuthDiplayed = true"> {{ loggedInUser ? 'Logout' : 'Login' }}</NavButton>
+        <span v-if="loggedInUser"> Logged in User: <b>{{ loggedInUser.login }}</b></span>
+        <NavButton v-if="loggedInUser" to="/account"> My Account</NavButton>
       </nav>
   </header>
 
@@ -26,67 +26,17 @@ apiStore.refresh();
 </template>
 
 <style scoped>
-/*
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    display: flex;
+    gap: 1em;
+    background-image: linear-gradient(90deg, white 20%, gray 70% );
+    border: 3px solid rgb(0, 204, 255);
+    border-radius: 15px;
+    padding: 0.5em;
   }
-}
-*/
+
+  nav > span {
+    padding: 0.5em;
+  }
+
 </style>
---
+++ front/src/components/NavButton.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+
+  const props = defineProps<{
+    to?: string,
+  }>();
+</script>
+
+<template>
+
+<RouterLink class="nav-button" :to="to" v-if="to">
+  <div>
+    <slot></slot>
+  </div>
+  <div class="bar" />
+</RouterLink>
+
+<div class="nav-button" v-else>
+  <div>
+    <slot></slot>
+  </div>
+  <div class="bar" />
+</div>
+
+</template>
+
+<style scoped>
+  .nav-button {
+    text-decoration: none;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em;
+    border-radius: 15px;
+    transition: 1s;
+    cursor: pointer;
+  }
+
+    .nav-button .bar {
+      background-image: linear-gradient(90deg, #0ea8e5, #2a2af1 85%);
+      height: 3px;
+      width: 0%;
+    }
+    nav .nav-button:hover .bar {
+      animation: nav-button-hover 1s linear both;
+    }
+
+  .nav-button:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  @keyframes nav-button-hover{
+    0%{
+      width: 0%;
+    }
+    30%{
+      width: 75%;
+    }
+    90%{
+      width: 93%;
+    }
+    100%{
+      width: 100%;
+    }
+  };
+</style>
