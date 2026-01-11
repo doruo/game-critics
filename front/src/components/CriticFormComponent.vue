61aef7c3d66a1a0e9e9ad6c638ca44f1a541674f
Yann Bodiguel <yann.bodiguel@etu.umontpellier.fr>
2026-01-11 14:46:40 +0100

    style (front): CSS CriticForm

--- front/src/components/CriticFormComponent.vue
+++ front/src/components/CriticFormComponent.vue
@@ -54,7 +54,22 @@ function uploadCritic() : void {
     <button type="button" @click="$emit('hideForm')"> Cancel</button>
     <button type="submit"> Upload</button>
   </form>
+  <br>
 </template>
 
 <style scoped>
+  textarea {
+    margin-left: 1em;
+    background-color: gray;
+    color: white;
+    padding: 0.3em;
+    border-radius: 5px;
+  }
+  form {
+    border-top: 4px solid rgb(0, 204, 255);
+    border-bottom: 4px solid rgb(0, 204, 255);
+    border-radius: 8px;
+    padding: 0.3em;
+    width: fit-content;
+  }
 </style>
