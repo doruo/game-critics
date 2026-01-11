a61139f79c96a846b61da8110edaf2e8f23c2779
Yann Bodiguel <yann.bodiguel@etu.umontpellier.fr>
2026-01-11 14:42:21 +0100

    style (front): CSS CriticList

--- front/src/components/CriticComponent.vue
+++ front/src/components/CriticComponent.vue
@@ -67,37 +67,65 @@ function cancelEdit() : void {
 
 <template>
   <div :class="(`${displayFor}-critic critic`)">
-    <hr>
-
     <button v-if="userIsAuthor && !isBeingEdited" @click="isBeingEdited = true"> Edit Critic</button>
     <button v-if="isBeingEdited" @click="saveEditedCritic"> Save</button>
     <button v-if="isBeingEdited" @click="cancelEdit"> Cancel</button>
     <!-- TODO : s'assurer que le role soit bien 'ADMIN' et pas autre chose -->
     <button v-if="userIsAuthor || loggedInUser?.roles.includes('ADMIN')" @click="deleteCrtic"> Delete Critic</button>
 
+    <p v-if="!isBeingEdited">Note : <b>{{ critic.note }}</b> Stars</p>
+    <p v-else>Note : <input v-model="editedCritic.note" type="number"> Stars</p>
+
+  <div class="messages">
+
     <p><b> General Critic :</b></p>
-    <p v-if="!isBeingEdited">{{ critic.generalMessage }}</p>
-    <textarea v-else v-model="editedCritic.generalMessage" ></textarea>
+    <p class="message" v-if="!isBeingEdited">{{ critic.generalMessage }}</p>
+    <textarea class="message" v-else v-model="editedCritic.generalMessage" ></textarea>
 
     <p><b> Visuals Critic :</b></p>
-    <p v-if="!isBeingEdited">{{ critic.visualMessage }}</p>
-    <textarea v-else v-model="editedCritic.visualMessage" ></textarea>
+    <p class="message" v-if="!isBeingEdited">{{ critic.visualMessage }}</p>
+    <textarea class="message" v-else v-model="editedCritic.visualMessage" ></textarea>
 
     <p><b> Soundtrack's Critic :</b></p>
-    <p v-if="!isBeingEdited">{{ critic.soundtrackMessage }}</p>
-    <textarea v-else v-model="editedCritic.soundtrackMessage" ></textarea>
+    <p class="message" v-if="!isBeingEdited">{{ critic.soundtrackMessage }}</p>
+    <textarea class="message" v-else v-model="editedCritic.soundtrackMessage" ></textarea>
 
     <p><b> Scenario's Critic :</b></p>
-    <p v-if="!isBeingEdited">{{ critic.scenarioMessage }}</p>
-    <textarea v-else v-model="editedCritic.scenarioMessage" ></textarea>
-
-    <p v-if="displayFor === 'game'"><i>Published by </i> <RouterLink :to="`/user/${critic.author.id}`"> {{ critic.author.login }}</RouterLink> at {{ (new Date(critic.publicationDate)).toLocaleString("fr") }}</p>
-    <p v-else><i>For the game : </i> <RouterLink :to="`/game/${critic.game.id}`"> {{ critic.game.name }}</RouterLink> at {{ (new Date(critic.publicationDate)).toLocaleString("fr") }}</p>
-
-    <p v-if="!isBeingEdited">Note : {{ critic.note }} Stars</p>
-    <p v-else>Note : <input v-model="editedCritic.note" type="number"> Stars</p>
+    <p class="message" v-if="!isBeingEdited">{{ critic.scenarioMessage }}</p>
+    <textarea class="message" v-else v-model="editedCritic.scenarioMessage" ></textarea>
   </div>
+
+    <p v-if="displayFor === 'game'"><i>Published by </i> <RouterLink :to="`/user/${critic.author.id}`"> {{ critic.author.login }}</RouterLink> on {{ (new Date(critic.publicationDate)).toLocaleString("fr") }}</p>
+    <p v-else><i>For the game : </i> <RouterLink :to="`/game/${critic.game.id}`"> {{ critic.game.name }}</RouterLink> on {{ (new Date(critic.publicationDate)).toLocaleString("fr") }}</p>
+
+  </div>
+  <br>
 </template>
 
 <style scoped>
+  .critic {
+    border-top: 4px solid rgb(0, 204, 255);
+    border-bottom: 4px solid rgb(0, 204, 255);
+    border-radius: 8px;
+    padding: 0.3em;
+  }
+
+  .messages {
+    border: 2px solid black;
+    border-radius: 15px;
+    max-width: 100%;
+    min-width: 700px;
+    padding: 0.5em;
+  }
+
+  .messages .message {
+    margin-left: 1em;
+    background-color: gray;
+    color: white;
+    padding: 0.3em;
+    border-radius: 5px;
+  }
+  .messages textarea {
+    width: 90%;
+  }
 </style>
